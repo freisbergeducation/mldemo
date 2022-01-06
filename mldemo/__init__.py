594cd0a99e0b6abc948942447d6bec44691bc67a
Benjamin Freisberg <freisberg@gibb.ch>
2022-01-06 22:35:06 +0100

    added port to __init__.py

--- mldemo/__init__.py
+++ mldemo/__init__.py
@@ -6,19 +6,3 @@ app = Flask(__name__, instance_relative_config=True)
 @app.route('/')
 def index():
     return render_template("index.html")
-
-"""if __name__ == '__main__':
-    PORT = int(os.getenv('PORT'))
-
-def create_app():
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-
-    @app.route('/')
-    def index():
-        return render_template("index.html")
-    
-    if __name__ == '__main__':
-        PORT = int(os.getenv('PORT'))
-
-    return app"""
