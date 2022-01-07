a9466b239f199452637c2acd2f7128f0ed018e02
Benjamin Freisberg <freisberg@gibb.ch>
2022-01-07 14:28:19 +0100

    added photo upload

--- mldemo/__init__.py
+++ mldemo/__init__.py
@@ -1,8 +1,18 @@
-from flask import Flask, render_template
-import os
+from flask import Flask, render_template, request, url_for
+from PIL import Image
 
 app = Flask(__name__, instance_relative_config=True)
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    textinput = request.form['textinput']
+    imageinput = request.files['imageinput']
+    img = Image.open(imageinput)
+    img = img.save("image.jpeg")
+    return render_template("index.html", textinput=textinput, imageinput=imageinput, img=img)
+
+# split this up in multiple python files
