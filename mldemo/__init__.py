06638dc3288878745fee16b97db7e8f7c2898c68
Benjamin Freisberg <bfreisberg@gmail.com>
2022-03-31 14:30:08 +0200

    debugging audio...

--- mldemo/__init__.py
+++ mldemo/__init__.py
@@ -144,7 +144,7 @@ def result_audio():
   random_nr = randint(1000, 9999)
   audio_file_path = os.path.join('audio/', 'audio_input_' + str(random_nr) + '.m4a')
   audio_file_path_wav = os.path.join('audio/', 'audio_input_' + str(random_nr) + '.wav')
-  app.logger.into("######################## 1 ############################")
+  app.logger.info("######################## 1 ############################")
   audio_input.save(audio_file_path)
 
   app.logger.info(os.path.exists(audio_file_path))
